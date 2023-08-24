2587d8a02aa855a90a8bc57404f4a83311051643
Cloud User <ec2-user@ip-172-31-18-213.ec2.internal>
2023-08-24 11:29:18 +0000

    exercício 1 feito

---
+++ Lab1b-24-agosto/exercicio1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  int menor = 10;
+  int linha = 0;
+  int matriz[3][3] = {{1,5,6},
+                      {8,9,7},
+                      {2,3,4}};
+  
+  for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+      if (matriz[i][j] < menor){
+        menor = matriz[i][j];
+        linha = i+1;
+      }
+    }
+  }
+  printf("elemento "%d na linha %d\n", menor, linha);
+  return 0;
+}
