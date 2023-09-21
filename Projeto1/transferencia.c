903353b0a99d358bd1c950c26b8d83c987ca5383
Cloud User <ec2-user@ip-172-31-16-240.ec2.internal>
2023-09-21 13:12:08 +0000

    Correção do código

--- Projeto1/transferencia.c
+++ Projeto1/transferencia.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
-
-#define MAX 100
+#include <time.h>
 
 pthread_mutex_t mutex;
 
@@ -12,38 +11,50 @@ typedef struct {
 
 void* transferencia(void* arguments) {
     conta** contas = (conta**)arguments;
+   
+    int fluxo = rand() % 2;
+  
     pthread_mutex_lock(&mutex);
 
-    if ((*contas[0]).saldo < (*contas[2]).saldo) {
+    if ((*contas[fluxo]).saldo < ((*contas[2]).saldo)) {
 	pthread_mutex_unlock(&mutex);
 	return NULL;
     }
 
-    (*contas[0]).saldo -= (*contas[2]).saldo;
-    (*contas[1]).saldo += (*contas[2]).saldo;
+    (*contas[fluxo]).saldo -= (*contas[2]).saldo;
+    (*contas[abs(fluxo-1)]).saldo += (*contas[2]).saldo;
+    (*contas[3 + fluxo]).saldo += 1;
+
     pthread_mutex_unlock(&mutex);
+
     return NULL;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 4 || strtol(argv[1], NULL, 10) > MAX || (strtol(argv[3], NULL, 10) != 0 && strtol(argv[3], NULL, 10) != 1)) {
-        fprintf(stderr, "Arg1 = Quantidade de transações. Arg2 = Valor das transações. Arg3 = 0 ou 1\n");
+    if (argc != 3) {
+        fprintf(stderr, "Arg1 = Quantidade de transações. Arg2 = Valor das transações\n");
         return 1;
     }
 
     int num = strtol(argv[1], NULL, 10);
     float quantia = strtof(argv[2], NULL);
-    int opcao = strtol(argv[3], NULL, 10);
 
-    conta from, to, quantias;
+    conta from, to, quantias, from_to, to_from;
     from.saldo = 100;
     to.saldo = 100;
     quantias.saldo = quantia;
+    from_to.saldo = 0;
+    to_from.saldo = 0;
 
-    conta ** arguments = (conta**) malloc(argc * sizeof(conta*));
-    arguments[opcao] = &from;
-    arguments[abs(opcao - 1)] = &to;
+    conta ** arguments = (conta**) malloc(5 * sizeof(conta*));
+  
+    arguments[0] = &from;
+    arguments[1] = &to;
     arguments[2] = &quantias;
+    arguments[3] = &from_to;
+    arguments[4] = &to_from;
+   
+    srand(time(NULL));
 
     pthread_t* thread_array = malloc(num * sizeof(pthread_t));
     if (pthread_mutex_init(&mutex, NULL) != 0) {
@@ -59,11 +70,14 @@ int main(int argc, char* argv[]) {
         pthread_join(thread_array[i], NULL);
     }
 
+    printf("From -> To = %.2f\n", from_to.saldo);
+    printf("To -> From = %.2f\n\n", to_from.saldo);
     printf("Conta from: %.2f\n", from.saldo);
     printf("Conta to: %.2f\n", to.saldo);
 
     pthread_mutex_destroy(&mutex);
     free(thread_array);
+    free(arguments);
 
     return 0;
 }
