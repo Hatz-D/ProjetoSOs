a20cc4e782c5850cfffd28385ab8112a1c7869cf
Cloud User <ec2-user@ip-172-31-16-240.ec2.internal>
2023-10-26 11:32:56 +0000

    wo feito

--- Lab3b-26-outubro/pi-wo-mutex.c
+++ Lab3b-26-outubro/pi-wo-mutex.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
-int pi = 0;
+double pi = 0;
 
-void* calculaPi() {
-	
+void* calculaPi(void* arguments) {
+    int* args = (int*) arguments;
+    int n = args[0];
+    
+    if(n % 2 == 0) {
+    	pi += 1.0/(2 * n + 1);
+    }
+
+    else {
+	pi += -1 * (1.0/(2 * n + 1));    
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -18,10 +28,15 @@ int main(int argc, char* argv[]) {
     pthread_t* thread_array = malloc(n * sizeof(pthread_t));
 
     for (int i = 0; i < n; i++) {
-        pthread_create(&thread_array[i], NULL, calculaPi, NULL);
+	int * arguments = (int*) malloc(sizeof(int));
+	arguments[0] = i;
+        pthread_create(&thread_array[i], NULL, calculaPi, (void*)arguments);
     }
 
     for (int i = 0; i < n; i++) {
         pthread_join(thread_array[i], NULL);
     }
+
+    pi = 4 * pi;
+    printf("\nNúmero de threads: %d\nValor de pi: %lf\n", n, pi);
 }
