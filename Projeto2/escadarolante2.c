3e003fb8ea861655a3cfc957118bd2b2d2de4d6f
Cloud User <ec2-user@ip-172-31-16-240.ec2.internal>
2023-10-31 18:21:14 +0000

    Correção de bugs

--- Projeto2/escadarolante2.c
+++ Projeto2/escadarolante2.c
@@ -14,11 +14,11 @@ int main(int argc, char* argv[]) {
 	int* instantes = (int*) malloc(N * sizeof(int));
 	int* fluxos = (int*) malloc(N * sizeof(int));
 	int* computados = (int*) calloc(N, sizeof(int));
-	
+/*	
 	for(int i = 0; i < N; i++){
 		if(i == 0) {instantes[0] = rand() % 10;}
 		else {
-			int tempo_aleatorio = rand() % 20;
+			int tempo_aleatorio = rand() % 12;
 			if(tempo_aleatorio == 0 || tempo_aleatorio == 10) {tempo_aleatorio++;}
 			instantes[i] = instantes[i-1] + tempo_aleatorio;
 		}
@@ -27,15 +27,17 @@ int main(int argc, char* argv[]) {
 
 		printf("\n%d %d", instantes[i], fluxos[i]);
 	}
-	
-/*	
-	instantes[0] = 5;
-	instantes[1] = 10;
-	instantes[2] = 16;
+*/	
+
+	instantes[0] = 3;
+	instantes[1] = 6;
+	instantes[2] = 11;
+	instantes[3] = 17;
 	fluxos[0] = 0;
 	fluxos[1] = 1;
 	fluxos[2] = 0;
-*/	
+	fluxos[3] = 1;
+
 
 	int tempo_total = 0;
 	int contador = 10;
@@ -51,13 +53,21 @@ int main(int argc, char* argv[]) {
 			}
 					
 			computados[i] = 1;
+			contador = 10;
 		}
 
-		while(j < N && instantes[j] - instantes[i] < 10) {
+		while(j < N && contador - (instantes[j] - instantes[j-1]) > 0) {
 			if(fluxos[i] == fluxos[j] && computados[j] == 0) {
-				tempo_total = instantes[j] + 10;
+				if(tempo_total > instantes[j] + 10) {tempo_total += 0;}
+				else {tempo_total = instantes[j] + 10;}
+			
 				computados[j] = 1;
+				contador = 10;
 			}
+		
+		
+
+			else{contador -= instantes[j] - instantes[j-1];}
 
 			j++;
 		}
