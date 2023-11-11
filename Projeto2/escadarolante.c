580bd641b392fd516f04901ea2ab3c552f0bdeab
Cloud User <ec2-user@ip-172-31-16-240.ec2.internal>
2023-11-11 13:25:28 +0000

    Correção e geração dos logs

--- Projeto2/escadarolante.c
+++ Projeto2/escadarolante.c
@@ -1,49 +1,6 @@
 #include <stdio.h>
 #include <time.h>
-#include <pthread.h>
 #include <stdlib.h>
-
-int tempo = 0;
-
-pthread_mutex_t mutex;
-
-typedef struct {
-	int* instantes;
-	int* fluxos;
-	int i;
-} thread_arguments;
-
-void* pessoaNaEscada(void* arguments) {
-	thread_arguments* args = (thread_arguments*) arguments;
-
-	int* instantes = (*args).instantes;
-	int* fluxos = (*args).fluxos;
-	int i = (*args).i;
-
-	if(i == 0) {
-		pthread_mutex_lock(&mutex);
-		tempo += instantes[0] + 10;
-		pthread_mutex_unlock(&mutex);
-	}
-
-	else{
-		if(fluxos[i] == fluxos[i-1]) {
-			pthread_mutex_lock(&mutex);
-			tempo += instantes[i] - instantes[i-1];
-			pthread_mutex_unlock(&mutex);
-		}
-
-		else {
-			pthread_mutex_lock(&mutex);
-			tempo += instantes[i] - instantes[i-1] > 10 ? instantes[i] - instantes[i-1] : 10 + instantes[i] - instantes[i-1];
-			pthread_mutex_unlock(&mutex);
-
-		}
-	}
-
-	printf("\n%d %d", instantes[i], fluxos[i]);
-
-}
 
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
@@ -56,36 +13,73 @@ int main(int argc, char* argv[]) {
 
 	int* instantes = (int*) malloc(N * sizeof(int));
 	int* fluxos = (int*) malloc(N * sizeof(int));
-
+	int* computados = (int*) calloc(N, sizeof(int));
+	
 	for(int i = 0; i < N; i++){
 		if(i == 0) {instantes[0] = rand() % 10;}
 		else {
-			int tempo_aleatorio = rand() % 20;
-			instantes[i] = instantes[i-1] + (tempo_aleatorio == 10 || tempo_aleatorio == 0) ? tempo_aleatorio++ : tempo_aleatorio;
+			int tempo_aleatorio = rand() % 12;
+			if(tempo_aleatorio == 0 || tempo_aleatorio == 10) {tempo_aleatorio++;}
+			instantes[i] = instantes[i-1] + tempo_aleatorio;
+			int k = i - 1;
+
+			while(instantes[k] + 10 >= instantes[i] && k >= 0) {
+                                if(instantes[k] + 10 == instantes[i]) {
+					instantes[i] += 1;
+					break;
+				}
+
+				else{k--;}
+                        }
 		}
 
 		fluxos[i] = rand() % 2;
+
+		printf("\n%d %d", instantes[i], fluxos[i]);
+	}
+	
+	int tempo_total = 0;
+	int contador = 10;
+	int i = 0;
+	int j = 1;
+
+	while(i < N) {
+		if(computados[i] == 0) {
+			if(i == 0) {tempo_total += instantes[i] + 10;}
+			else {
+				if(tempo_total > instantes[i]) {tempo_total += 10;}
+				else {tempo_total = instantes[i] + 10;}
+			}
+					
+			computados[i] = 1;
+			contador = 10;
+		}
+
+		while(j < N ) {
+            		if(contador - (instantes[j] - instantes[j-1]) > 0 || tempo_total > instantes[j]) {
+              			if(fluxos[i] == fluxos[j] && computados[j] == 0) {
+                			if(tempo_total > instantes[j] + 10) {tempo_total += 0;}
+                			else if(tempo_total > instantes[j]) {tempo_total = instantes[j] + 10;}
+                			else {tempo_total += 10;}
+            
+                		computados[j] = 1;
+                		contador = 10;
+            			}
+        
+            			else{contador -= instantes[j] - instantes[j-1];}
+
+        	    		j++;
+        		}
+
+			else{break;}
+                   
+        	}
+	
+		i++;
+		j = i + 1;
 	}
 
-	pthread_t* thread_array = malloc(N * sizeof(pthread_t));
-
-	pthread_mutex_init(&mutex, NULL);
-
-	for(int i = 0; i < N; i++) {
-                thread_arguments* arguments = (thread_arguments*) malloc(sizeof(thread_arguments));
-
-		(*arguments).instantes = instantes;
-		(*arguments).fluxos = fluxos;
-		(*arguments).i = i;
-
-		pthread_create(&thread_array[i], NULL, pessoaNaEscada, (void*)arguments);
-	}
-
-	for(int i = 0; i < N; i++) {
-		pthread_join(thread_array[i], NULL);
-	}
-
-	printf("\n\nTempo final: %d\n", tempo);
+	printf("\n\nTempo final: %d\n", tempo_total);
 
 	return 0;
 }
