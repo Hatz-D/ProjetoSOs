b4d70c0455fc084825b4837c1f9e584489efc7c6
Cloud User <ec2-user@ip-172-31-33-90.ec2.internal>
2023-11-16 19:04:10 +0000

    Adicionando escadarolante2.c# Please enter the commit message for your changes. Lines starting

---
+++ Projeto2/escadarolante2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int instante;
+    int fluxo;
+} Pessoa;
+
+int main() {
+    int N;
+
+    printf("Quantidade de pessoas na escada rolante: ");
+    scanf("%d", &N);
+
+    Pessoa* pessoas = (Pessoa*)malloc(N * sizeof(Pessoa));
+
+    printf("Informe os dados para cada pessoa (instante fluxo):\n");
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &pessoas[i].instante, &pessoas[i].fluxo);
+    }
+
+    int tempo_total = 0;
+    int contador = 10;
+
+    for (int i = 0; i < N; i++) {
+        if (pessoas[i].instante == -1) continue; // Pessoa já considerada
+
+        tempo_total = (i == 0) ? pessoas[i].instante + 10 : (tempo_total > pessoas[i].instante) ? tempo_total + 10 : pessoas[i].instante + 10;
+        contador = 10;
+
+        for (int j = i + 1; j < N; j++) {
+            if (pessoas[j].instante == -1) continue; // Pessoa já considerada
+
+            if (contador - (pessoas[j].instante - pessoas[j - 1].instante) > 0 || tempo_total > pessoas[j].instante) {
+                if (pessoas[i].fluxo == pessoas[j].fluxo) {
+                    tempo_total = (tempo_total > pessoas[j].instante + 10) ? tempo_total : (tempo_total > pessoas[j].instante) ? pessoas[j].instante + 10 : tempo_total + 10;
+                    contador = 10;
+                    pessoas[j].instante = -1; // Marcar pessoa como considerada
+                } else {
+                    contador -= pessoas[j].instante - pessoas[j - 1].instante;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    printf("\n\nTempo final: %d\n", tempo_total);
+
+    free(pessoas);
+
+    return 0;
+}
